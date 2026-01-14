6cee2af99ded6d41cf4f275446aeaaacd9742a90
Shyam Sarkar <shyamsarkar@zohomail.in>
2026-01-14 13:57:45 +0530

    Added 004

---
+++ hashmap/004_169.rb
@@ -0,0 +1,48 @@
+169. Majority Element
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+The input is generated such that a majority element will exist in the array.
+ 
+
+Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+# @param {Integer[]} nums
+# @return {Integer}
+def majority_element(nums)
+    count = 0
+    selected = nil
+
+    nums.each do |num|
+        if count == 0
+            selected = num
+        end
+
+        if selected == num
+            count += 1
+        else
+            count -= 1
+        end
+    end
+
+    selected
+end
