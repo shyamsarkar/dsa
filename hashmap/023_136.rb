be26e09bd963fd5a4d4bb27bcea0b70e35747a82
Shyam Sarkar <shyamsarkar@zohomail.in>
2026-02-02 09:27:44 +0530

    Added 023

---
+++ hashmap/023_136.rb
@@ -0,0 +1,50 @@
+# 136. Single Number
+
+=begin
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,1]
+
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+
+Output: 1
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+ 
+Seen this question in a real interview before?
+1/5
+=end
+
+# @param {Integer[]} nums
+# @return {Integer}
+def single_number(nums)
+    result = 0
+
+    nums.each do |num|
+        result ^= num
+    end
+    
+    result
+end
