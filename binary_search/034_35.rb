be1c8b8f18468751e8a03d6c4457449f8f4400d7
Shyam Sarkar <shyamsarkar@zohomail.in>
2026-02-13 19:01:07 +0530

    Added 034

---
+++ binary_search/034_35.rb
@@ -0,0 +1,55 @@
+# 35. Search Insert Position
+
+=begin
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+ 
+Seen this question in a real interview before?
+1/5
+=end
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer}
+def search_insert(nums, target)
+  left = 0
+  right = nums.size - 1
+
+  while left <= right
+    mid = left + (right-left)/2
+
+    if nums[mid] == target
+      return mid
+    elsif nums[mid] < target
+      left = mid + 1
+    else
+      right = mid - 1
+    end
+  end
+
+  left
+end
